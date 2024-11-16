9eaa1c3ce61cb16aea348d2c9919c4a71de9d3ae
Abhay Tiwari <88594376+iabhaykaran@users.noreply.github.com>
2024-11-17 00:29:06 +0530

    msg

--- yotube/src/Routes/Routes.js
+++ yotube/src/Routes/Routes.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../Pages/Home";
-import VideoCard from "../components/VideoCard";
+// import VideoCard from "../components/VideoCard";
 
 export default function R() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<VideoCard />} />
+        <Route path="/" element={<Home/>} />
         <Route path="/home" element={<Home />} />
         {/* <Route index element={""} /> */}
         {/* <Route path="blogs" element={""} /> */}
