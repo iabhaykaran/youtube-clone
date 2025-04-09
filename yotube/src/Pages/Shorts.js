6aa233c885e0576d356a30a4814647db26bf5e30
iabhaykaran <iamabhaykaran@gmail.com>
2025-04-10 03:14:01 +0530

    msg

--- yotube/src/Pages/Shorts.js
+++ yotube/src/Pages/Shorts.js
@@ -20,22 +20,98 @@
 // };
 
 // export default Shorts;
-import React from "react";
-import Short from "../components/ShortsCard";
 
-const Shorts = () => {
+
+import React, { useRef, useEffect, useState } from "react";
+import { useSwipeable } from "react-swipeable";
+import ShortsData from "../ShortsData";
+
+const Short = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const videoRefs = useRef([]);
+
+  const handlers = useSwipeable({
+    onSwipedUp: () => handleSwipe("up"),
+    onSwipedDown: () => handleSwipe("down"),
+    preventDefaultTouchmoveEvent: true,
+    trackTouch: true,
+    trackMouse: false,
+  });
+
+  const handleSwipe = (direction) => {
+    setCurrentIndex((prevIndex) => {
+      if (direction === "up" && prevIndex < ShortsData.length - 1) {
+        return prevIndex + 1;
+      } else if (direction === "down" && prevIndex > 0) {
+        return prevIndex - 1;
+      }
+      return prevIndex;
+    });
+  };
+
+  useEffect(() => {
+    videoRefs.current.forEach((video, index) => {
+      if (video) {
+        if (index === currentIndex) {
+          video.play();
+        } else {
+          video.pause();
+        }
+      }
+    });
+  }, [currentIndex]);
+
   return (
     <div
+      {...handlers}
       style={{
         height: "100vh",
+        width: "100%",
         overflow: "hidden",
+        position: "relative",
       }}
     >
-      <div className="shorts-box">
-        <Short />
-      </div>
+      {ShortsData.map((video, index) => (
+        <div
+          key={video.id}
+          style={{
+            height: "100vh",
+            width: "100%",
+            position: "absolute",
+            top: `${(index - currentIndex) * 100}vh`,
+            left: 0,
+            transition: "top 0.3s ease-in-out",
+          }}
+        >
+          <video
+            ref={(el) => (videoRefs.current[index] = el)}
+            src={video.url}
+            controls={false}
+            loop
+            width="100%"
+            playsInline
+            muted
+            style={{ height: "100%", objectFit: "cover" }}
+          />
+          <div
+            className="overlay"
+            style={{
+              position: "absolute",
+              bottom: "20px",
+              left: "10px",
+              color: "white",
+            }}
+          >
+            <div style={{ display: "flex" }}>
+              <p style={{ margin: "5px" }}>{video.username}</p>
+              <p className="subs-btn"> Subscribe</p>
+            </div>
+            <p>#zimbabwe #victoriafalls #zambia</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
 
-export default Shorts;
+export default Short;
