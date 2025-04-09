c53ce3146daaee48eeaee217413a716708ee7945
iabhaykaran <iamabhaykaran@gmail.com>
2025-04-10 03:07:29 +0530

    msg

--- yotube/src/Pages/Shorts.js
+++ yotube/src/Pages/Shorts.js
@@ -1,16 +1,35 @@
+// import React from "react";
+// // import Reel from "../components/ShortsCard";
+// import Short from "../components/ShortsCard";
+// const Shorts = () => {
+//   return (
+//     <div
+//       style={{
+//         // display: "flex",
+//         // justifyContent: "center",
+//         // alignItems: "center",
+//         // overflowY: "scroll",
+//       }}
+//       className="scroll-none"
+//     >
+//       <div className="shorts-box">
+//         <Short />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Shorts;
 import React from "react";
-// import Reel from "../components/ShortsCard";
 import Short from "../components/ShortsCard";
+
 const Shorts = () => {
   return (
     <div
       style={{
-        // display: "flex",
-        // justifyContent: "center",
-        // alignItems: "center",
-        // overflowY: "scroll",
+        height: "100vh",
+        overflow: "hidden",
       }}
-      className="scroll-none"
     >
       <div className="shorts-box">
         <Short />
